64fa1e0f81abea422e6759a2e4e5687caf1b942c
h0000n <anjunghoon2716@gmail.com>
2023-06-25 20:34:18 +0900

    closed #21 인벤토리 추가,  feat: PassiveSkill

--- Assets/Scripts/Skill/PassiveSkillRead.cs
+++ Assets/Scripts/Skill/PassiveSkillRead.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 using System.Text.RegularExpressions;
 using System;
 
-public class PassiveTest : MonoBehaviour
+public class PassiveSkillRead : MonoBehaviour
 {
     public List<Dictionary<string, object>> PassiveSkillCsv;
     public Dictionary<string, PassiveSkill> PassiveSkillData = new Dictionary<string, PassiveSkill>();
+    public Dictionary<string, Melody> PassiveMelody = new Dictionary<string, Melody>();
     void Awake()
     {
         PassiveSkillCsv = CSVReader.Read("PassiveSkillData");
@@ -15,6 +16,7 @@ public class PassiveTest : MonoBehaviour
         {
             string name = PassiveSkillCsv[i]["name"].ToString();
             string info = PassiveSkillCsv[i]["info"].ToString();
+            int tier = Convert.ToInt32(PassiveSkillCsv[i]["tier"]);
             float hp = Convert.ToSingle(PassiveSkillCsv[i]["hp"]);
             float damage = Convert.ToSingle(PassiveSkillCsv[i]["damage"]);
             float speed = Convert.ToSingle(PassiveSkillCsv[i]["speed"]);
@@ -22,7 +24,8 @@ public class PassiveTest : MonoBehaviour
             float defence = Convert.ToSingle(PassiveSkillCsv[i]["defence"]);
             float pierce = Convert.ToSingle(PassiveSkillCsv[i]["pierce"]);
 
-            PassiveSkillData.Add(PassiveSkillCsv[i]["name"].ToString(), new PassiveSkill(name, info, hp, damage, speed, attackspeed, defence, pierce));
+            PassiveSkillData.Add(name, new PassiveSkill(name, info, tier, hp, damage, speed, attackspeed, defence, pierce));
+            PassiveMelody.Add(name, new Melody(name, PassiveSkillData[name]));
         }
     }
 }
